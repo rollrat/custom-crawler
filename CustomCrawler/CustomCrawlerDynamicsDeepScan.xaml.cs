492a24b81ef9cacd84217e66bd685015fd7bd4d0
rollrat <rollrat.cse@gmail.com>
2020-04-15 19:06:41 +0900

    dynamic: Add deep-scan window not implemented

---
+++ CustomCrawler/CustomCrawlerDynamicsDeepScan.xaml.cs
@@ -0,0 +1,35 @@
+﻿/***
+
+   Copyright (C) 2020. rollrat. All Rights Reserved.
+   
+   Author: Custom Crawler Developer
+
+***/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CustomCrawler
+{
+    /// <summary>
+    /// CustomCrawlerDynamicsDeepScan.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class CustomCrawlerDynamicsDeepScan : Window
+    {
+        public CustomCrawlerDynamicsDeepScan()
+        {
+            InitializeComponent();
+        }
+    }
+}
